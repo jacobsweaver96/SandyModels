33664c09d7dbd75c17cbd75824213f812f88a6c8
jacobsweaver96 email = jacobsweaver@gmail.com <jacobsweaver@gmail.com>
2017-10-18 21:09:32 -0400

    Added more fields to users;

---
+++ SandyModels/Models/RelevantLink.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Serialization;
+
+namespace SandyModels.Models
+{
+    /// <summary>
+    /// A serializable class for representing a link that is to be displayed on a user's profile page
+    /// </summary>
+    [DataContract]
+    public class RelevantLink
+    {
+        /// <summary>
+        /// The relevant link id
+        /// </summary>
+        [DataMember]
+        public int RelevantLinkId { get; set; }
+
+        /// <summary>
+        /// The link uri
+        /// </summary>
+        [DataMember]
+        public string LinkUri { get; set; }
+
+        /// <summary>
+        /// The id of the associated user
+        /// </summary>
+        [DataMember]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Whether or not this link has been removed
+        /// </summary>
+        [DataMember]
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// The link text to be displayed
+        /// </summary>
+        [DataMember]
+        public string UrlText { get; set; }
+    }
+}
--- SandyModels/Models/User.cs
+++ SandyModels/Models/User.cs
@@ -17,6 +17,42 @@ namespace SandyModels.Models
         public int UserId { get; set; }
 
         /// <summary>
+        /// The first name of the user
+        /// </summary>
+        [DataMember]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The middle name of the user
+        /// </summary>
+        [DataMember]
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// The last name of the user
+        /// </summary>
+        [DataMember]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The uri to the profile picture used by the user in their profile page
+        /// </summary>
+        [DataMember]
+        public string ProfilePictureUri { get; set; }
+
+        /// <summary>
+        /// A link to the user's resume
+        /// </summary>
+        [DataMember]
+        public string ResumeLink { get; set; }
+
+        /// <summary>
+        /// The user's biography summary
+        /// </summary>
+        [DataMember]
+        public string Biography { get; set; }
+
+        /// <summary>
         /// The user's Email
         /// </summary>
         [DataMember]
@@ -38,7 +74,19 @@ namespace SandyModels.Models
         /// The user's roles
         /// </summary>
         [DataMember]
-        public List<Role> Roles { get; set; } = new List<Role>();
+        public List<Role> Roles { get; set; }
+
+        /// <summary>
+        /// A list of github user accounts associated with this portfolio user
+        /// </summary>
+        [DataMember]
+        public List<GithubUser> GithubUsers { get; set; }
+
+        /// <summary>
+        /// A list of links that are to be displayed on this user's profile page
+        /// </summary>
+        [DataMember]
+        public List<RelevantLink> RelevantLinks { get; set; }
 
         /// <summary>
         /// Whether or not the user has been deleted
